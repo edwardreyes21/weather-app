bf08930065676876db7a64ce495229f25a4dbc63
Edward <edwardi.reyes21@gmail.com>
2021-10-27 19:05:28 -0700

    Added async function to get weather info

--- src/weather.js
+++ src/weather.js
@@ -1,12 +1,16 @@
-function getWeather() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4582f42658211134cda9749fe21a3065',
-        {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            console.log(response);
-        })
+import getCity from "./city";
+
+// Asynchronous function to get the weather
+async function getWeather() {
+    // Stores whatever is returned from the 'fetch' promise into a variable 'results'
+    const results = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4582f42658211134cda9749fe21a3065',
+        {mode: 'cors'});
+
+    // .json() is a promise, so whatever it returns upon being resolved is stored into 'response'
+    const response = await results.json();
+
+    // Finally, logs the contents of 'response' into console
+    console.log(response);
 }
 
 export default getWeather;
