060601690016ece393d6f1d7ede94538fde1eb2b
edwardreyes21 <edwardi.reyes21@gmail.com>
2021-10-28 18:21:18 -0700

    Added header, footer, and content areas

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import getWeather from "./weather";
 import {initializeHeader, initializeContent, initializeFooter} from "./website";
 
 initializeHeader();
+initializeContent();
+initializeFooter();
 getWeather();
--- src/website.js
+++ src/website.js
@@ -1,6 +1,6 @@
 function initializeHeader() {
     const header = document.querySelector('.header');
-    
+
     const header_text = document.createElement('h1');
     header_text.classList.add("header-text");
     header_text.textContent = "Weather App";
@@ -13,7 +13,13 @@ function initializeContent() {
 }
 
 function initializeFooter() {
+    const footer = document.querySelector('.footer');
 
+    const footer_text = document.createElement('h1');
+    footer_text.classList.add("footer-text");
+    footer_text.textContent = "Made by Edward";
+
+    footer.appendChild(footer_text);
 }
 
 export {initializeHeader, initializeContent, initializeFooter};
