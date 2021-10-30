1d383d36dce4c6c1b74e34c39afad3806d1a3b5d
edwardreyes21 <edwardi.reyes21@gmail.com>
2021-10-29 18:08:56 -0700

    Modified getWeather() to take in a parameter 'location'

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,9 @@
 import getCity from "./city";
 
 // Asynchronous function to get the weather
-async function getWeather() {
+async function getWeather(location) {
     // Stores whatever is returned from the 'fetch' promise into a variable 'results'
-    const results = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=4582f42658211134cda9749fe21a3065',
+    const results = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=4582f42658211134cda9749fe21a3065',
         {mode: 'cors'});
 
     // .json() is a promise, so whatever it returns upon being resolved is stored into 'response'
