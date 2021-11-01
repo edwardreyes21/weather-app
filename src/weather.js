b94a2f1fa2bec8fd3942e14838764d0f3e55cff0
edwardreyes21 <edwardi.reyes21@gmail.com>
2021-10-31 18:43:25 -0700

    Changed HTTP to HTTPS

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,7 @@
 // Asynchronous function to get the weather
 async function getWeather(location) {
     // Stores whatever is returned from the 'fetch' promise into a variable 'results'
-    const results = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=4582f42658211134cda9749fe21a3065',
+    const results = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=4582f42658211134cda9749fe21a3065',
         {mode: 'cors'});
 
     // .json() is a promise, so whatever it returns upon being resolved is stored into 'response'
